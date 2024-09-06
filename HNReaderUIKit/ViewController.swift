0fa25fcd5096281228a787c4694d218528c2ea86
Sargis Khachatryan <khachatrian.sargis@gmail.com>
2024-09-06 16:35:26 +0400

    added a new target for UIKit app

---
+++ HNReaderUIKit/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  HNReaderUIKit
+//
+//  Created by Sargis Khachatryan on 06.09.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
