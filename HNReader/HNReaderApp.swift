671a4bd6e0785f3fd42a84aafaab07e14c084144
Sargis Khachatryan <khachatrian.sargis@gmail.com>
2024-09-06 16:00:04 +0400

    initial commit

---
+++ HNReader/HNReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  HNReaderApp.swift
+//  HNReader
+//
+//  Created by Sargis Khachatryan on 06.09.24.
+//
+
+import SwiftUI
+
+@main
+struct HNReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
